d44b58d3cfa1b10376a72ca1bf758a75d66547ae
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2021-04-09 14:33:33 -0500

    Initial commit, basic structure

---
+++ src/main/java/com/sei/redditlikeapi/RedditlikeapiApplication.java
@@ -0,0 +1,13 @@
+package com.sei.redditlikeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditlikeapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditlikeapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sei/redditlikeapi/RedditlikeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sei.redditlikeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditlikeapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
