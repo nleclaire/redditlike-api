f8ba739c4f32b864a0018929aded6d289aed7019
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2021-04-09 15:24:52 -0500

    Topic Added

---
+++ src/main/java/com/sei/redditlikeapi/model/Topic.java
@@ -0,0 +1,51 @@
+package com.sei.redditlikeapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="topics")
+public class Topic {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    public Topic() {
+    }
+
+    public Topic(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
