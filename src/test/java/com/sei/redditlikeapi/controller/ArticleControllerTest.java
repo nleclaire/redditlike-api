bf52d159a9a9bd2471f61840216b98f58fe1f2c5
nleclaire <noah.leclaire@gmail.com>
2021-04-13 16:45:38 -0400

    Adding tests for TopicController, ArticleController, CommentsController, and UserController

--- src/test/java/com/sei/redditlikeapi/controller/ArticleControllerTest.java
+++ src/test/java/com/sei/redditlikeapi/controller/ArticleControllerTest.java
@@ -1,4 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.sei.redditlikeapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
 class ArticleControllerTest {
-  
+    @Autowired
+    private ArticleController controller;
+
+    @Test
+    public void contextLoads(){
+        assertThat(controller).isNotNull();
+    }
+
 }
--- src/test/java/com/sei/redditlikeapi/controller/UserControllerTest.java
+++ src/test/java/com/sei/redditlikeapi/controller/UserControllerTest.java
@@ -1,4 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.sei.redditlikeapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class UserControllerTest {
-  
+
+    @Autowired
+    private UserController controller;
+
+    @Test
+    public void contextLoads(){
+        assertThat(controller).isNotNull();
+    }
+
 }
