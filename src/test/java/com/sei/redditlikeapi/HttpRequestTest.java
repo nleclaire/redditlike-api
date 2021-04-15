9bd44bcf5417508e11bff58249df81daddb5cf60
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2021-04-15 12:28:42 -0500

    HttpRequestTest fixed

--- src/test/java/com/sei/redditlikeapi/HttpRequestTest.java
+++ src/test/java/com/sei/redditlikeapi/HttpRequestTest.java
@@ -6,16 +6,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
@@ -31,22 +36,12 @@ public class HttpRequestTest {
     @Autowired
     private TopicRepository topicRepository;
 
-    @BeforeEach
-    public void setUp() {
-        postTopicShouldReturnOKForASingleTopicAndIsOfClassTypeTopic();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        topicRepository.deleteById(topic.getId());
-    }
-
     // Create HttpEntity saved my life
     // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpEntity.html
     private HttpEntity createHttpEntityWithTopic(Topic topic){
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1bmNoLmxlY2xhaXJlQGVtYWlsLmNvbSIsImV4cCI6MTYxODUyOTI2NywiaWF0IjoxNjE4NDkzMjY3fQ.OoaL2Opo-QAS8DNcS85vtgfFFZBCe1cz3VYd02Hleec");
+        headers.setBearerAuth("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ2aWt0b3Iub2xlc25ldnljaEBob3RtYWlsLmNvbSIsImV4cCI6MTYxODU0MzAzMiwiaWF0IjoxNjE4NTA3MDMyfQ.PgmSGbOudHhehOZZ5TQ1hIzqjnP07q4Ev9yIexFybsQ");
 
         return new HttpEntity<>(topic, headers);
     }
@@ -54,7 +49,7 @@ public class HttpRequestTest {
     private HttpEntity createHttpRequest(){
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1bmNoLmxlY2xhaXJlQGVtYWlsLmNvbSIsImV4cCI6MTYxODUyOTI2NywiaWF0IjoxNjE4NDkzMjY3fQ.OoaL2Opo-QAS8DNcS85vtgfFFZBCe1cz3VYd02Hleec");
+        headers.setBearerAuth("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ2aWt0b3Iub2xlc25ldnljaEBob3RtYWlsLmNvbSIsImV4cCI6MTYxODU0MzAzMiwiaWF0IjoxNjE4NTA3MDMyfQ.PgmSGbOudHhehOZZ5TQ1hIzqjnP07q4Ev9yIexFybsQ");
 
         return new HttpEntity<>(headers);
     }
@@ -62,26 +57,21 @@ public class HttpRequestTest {
     @Test
     public void postTopicShouldReturnOKForASingleTopicAndIsOfClassTypeTopic(){
         String resourceURL = "http://localhost:" + port + "/api/topics/new";
-//        topic = new Topic("Test", "Test description");
+        topic = new Topic("Test", "Test description");
 
         HttpEntity<?> request = createHttpEntityWithTopic(topic);
         ResponseEntity<?> response = restTemplate.exchange(resourceURL, HttpMethod.POST, request, Topic.class);
 
-//        id = topic.getId();
-
         assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
         assertThat(response.getBody()).hasFieldOrPropertyWithValue("name", "Test");
         assertThat(response.getBody()).hasFieldOrPropertyWithValue("description", "Test description");
-//        assertThat(response.getBody()).hasSameClassAs(topic);
     }
-
 
     @Test
     public void getTopicShouldReturnASingleTopic(){
-//        id = topicRepository.findByName("Test").getId();
-        System.out.println(topic);
 
-        String resourceURL = "http://localhost:" + port + "/api/topics/" + topic.getId();
+        Long id = topicRepository.findByName("Test").getId();
+        String resourceURL = "http://localhost:" + port + "/api/topics/" + id;
         HttpEntity<?> request = createHttpRequest();
         ResponseEntity<?> response = restTemplate.exchange(resourceURL, HttpMethod.GET, request, Topic.class);
 
@@ -93,30 +83,24 @@ public class HttpRequestTest {
 
     @Test
     public void putTopicShouldReturnASingleTopicWithUpdatedFields(){
-//        id = topicRepository.findByName("Test").getId();
-
-        String resourceURL = "http://localhost:" + port + "/api/topics/" + topic.getId();
-
-        topic = new Topic("Update Test", "Update description");
+        Long id = topicRepository.findByName("Test").getId();
+        String resourceURL = "http://localhost:" + port + "/api/topics/" + id;
+        topic = new Topic("Test", "Update Description");
         HttpEntity<?> request = createHttpEntityWithTopic(topic);
         ResponseEntity<?> response = restTemplate.exchange(resourceURL, HttpMethod.PUT, request, Topic.class);
-//        topicRepository.save(topic);
-
-//        topic.setId(restTemplate.exchange(resourceURL, HttpMethod.GET, request, Topic.class).getBody().getId());
 
         assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
-        assertThat(response.getBody()).hasFieldOrPropertyWithValue("name", "Update Test");
-        assertThat(response.getBody()).hasFieldOrPropertyWithValue("description", "Update description");
+        assertThat(response.getBody()).hasFieldOrPropertyWithValue("name", "Test");
+        assertThat(response.getBody()).hasFieldOrPropertyWithValue("description", "Update Description");
     }
 
     @Test
     public void deleteTopicShouldReturnAResponseEntityWithFieldResponse(){
-//        id = topicRepository.findByName("Update Test").getId();
-
-        String resourceURL = "http://localhost:" + port + "/api/topics/" + topic.getId();
-        HttpEntity<?> request = createHttpEntityWithTopic(topic);
-        ResponseEntity<?> response = restTemplate.exchange(resourceURL, HttpMethod.DELETE, request, Topic.class);
+        Long id = topicRepository.findByName("Test").getId();
+        String resourceURL = "http://localhost:" + port + "/api/topics/" + id;
+        HttpEntity<?> request = createHttpRequest();
+        ResponseEntity<?> response = restTemplate.exchange(resourceURL, HttpMethod.DELETE, request, Object.class);
         assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
-        assertThat(response.getBody()).hasFieldOrProperty("response");
+        assertThat(response.getBody()).hasFieldOrProperty("Response");
     }
 }
