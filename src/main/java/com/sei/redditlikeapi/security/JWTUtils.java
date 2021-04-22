0bceefcc6912b1ee283b56763f500e3a9693e708
nleclaire <noah.leclaire@gmail.com>
2021-04-22 16:46:49 -0400

    Configuring application for prod

--- src/main/java/com/sei/redditlikeapi/security/JWTUtils.java
+++ src/main/java/com/sei/redditlikeapi/security/JWTUtils.java
@@ -3,6 +3,7 @@ package com.sei.redditlikeapi.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,10 @@ import java.util.function.Function;
 
 @Service
 public class JWTUtils {
-    String SECRET_KEY = "TestKey"; //Secret key to make token
+    //Secret key to make token
+    @Value("${jwt.secret}")
+    String SECRET_KEY;
+
     //Token Generation staring
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
